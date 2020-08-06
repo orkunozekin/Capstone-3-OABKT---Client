1d734cad755c4b8fd433aa3f9886a55e9f562f40
Thomas Davis <ThomasDavisA@gmail.com>
2020-08-06 08:01:24 -0700

    updated curse route

--- src/routes/CurseRoute/CurseRoute.js
+++ src/routes/CurseRoute/CurseRoute.js
@@ -1,20 +1,44 @@
 import React, { Component } from 'react';
-import CurseForm from '../../components/CurseForm/CurseForm.js';
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
+import config from '../../config';
+import TokenService from '../../services/token-service';
+import CurseForm from '../../components/CurseForm/CurseForm';
 import './CurseRoute.css';
 
 class CurseRoute extends Component {
 
-    handleClickEvent = (event) => {
-        event.preventDefault();
-        // logic to send curse to void
+
+
+    handlePostCurses = (ev) => {
+        ev.preventDefault()
+        const { curseInput } = ev.target
+        const curse = curseInput.value;
+        fetch(`${config.API_ENDPOINT}/curses`, {
+            method: 'POST',
+            headers: {
+                'authorization': `bearer ${TokenService.getAuthToken()}`,
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({
+                message: 'Curse sent annonymously',
+                curse
+            })
+        })
+            .then(res => {
+                if (res.ok) {
+                    return res.json();
+                }
+            })
+            .then(json => {
+                console.log(json)
+            })
     }
 
     render() {
         return (
             <div className='curse-bless-field'>
                 <h2 className='curse-bless-title'>Perform a Curse</h2>
-                <CurseForm onClick={e => this.handleClickEvent(e)}></CurseForm>
+                <CurseForm onSubmit={this.handlePostCurses}></CurseForm>
                 <Link className="link-login" to='/login'>...or login here</Link>
             </div>
 
