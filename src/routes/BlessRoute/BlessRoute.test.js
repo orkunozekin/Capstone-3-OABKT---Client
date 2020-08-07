30d13e1a937f5f27efbbdbda3a2cbcde88aeecfa
Orkun Ozekin <orkunozekin@thinkful.com>
2020-08-07 10:29:06 -0500

    smoke tests

--- src/routes/BlessRoute/BlessRoute.test.js
+++ src/routes/BlessRoute/BlessRoute.test.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Header from './Header';
+import BlessRoute from './BlessRoute';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Router><Header /></Router>, div);
+    ReactDOM.render(<Router><BlessRoute /></Router>, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 
--- src/routes/CurseRoute/CurseRoute.test.js
+++ src/routes/CurseRoute/CurseRoute.test.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Header from './Header';
+import CurseRoute from './CurseRoute';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Router><Header /></Router>, div);
+    ReactDOM.render(<Router><CurseRoute /></Router>, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 
