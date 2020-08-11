4e5d468b0dba3f00de4adf84959196bf19950988
Keith Freitag <keithfreitag@gmail.com>
2020-08-11 09:13:07 -0700

    landing page buttons

---
+++ src/routes/AlertBox/AlertBox.js
@@ -0,0 +1,29 @@
+// yadda yadda imports do this later wwhatevver
+
+
+
+
+
+
+
+render(){
+return (
+<div className='alert-box'>
+                <h2>About the App</h2>
+                <p>Our app gives users a way to curse in a 
+                    very anonymous way, curse while logged in, 
+                    and bless up to 3 times per day</p>
+
+                <h2>Please note</h2>
+                <p>We ask that no racist or bigoted
+                    language be used.  We also ask that 
+                    you disable cookies to prevent crashes.
+                </p>
+
+                <h2 className="Important">IMPORTANT</h2>
+                <p>ENTERTAINMENT PURPOSES ONLY</p>
+                <p>If you are experiencing mental depression or 
+                    other mental problems 
+                    PLEASE GET PROFESSIONAL HELP</p>
+)
+}
--- src/routes/LandingPageRoute/LandingPageRoute.js
+++ src/routes/LandingPageRoute/LandingPageRoute.js
@@ -1,31 +1,46 @@
+// disableCookies()
+// if (cookies === true) {
+//  throw new Error  
+// }
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import { render } from 'react-dom'
 import "./LandingPage.css"
+import AppContext from '../../contexts/AppContext'
+import Bless from '../BlessRoute/BlessRoute'
+import Curse from '../../components/CurseForm/CurseForm'
+import Button from '../../components/Button/Button'
 
 class LandingPageRoute extends Component {
+    static contextType = AppContext;
+
+    loggedInBlessPart = () => {
+        if (this.context.user) {
+            return <>
+               <Link component={Bless}>
+                   <Button className='blessText'>
+                       <h2>Bless</h2>
+                   </Button>
+               </Link>
+            </>
+        }
+    }
+
+
+
+
 
     render() {
         return (
-            <div className='alert-box'>
-                <h2>About the App</h2>
-                <p>Our app gives users a way to curse in a 
-                    very anonymous way, curse while logged in, 
-                    and bless up to 3 times per day</p>
-
-                <h2>Please note</h2>
-                <p>We ask that no racist or bigoted
-                    language be used.
-                </p>
-
-                <h2 className="Important">IMPORTANT</h2>
-                <p>ENTERTAINMENT PURPOSES ONLY</p>
-                <p>If you are experiencing mental depression or 
-                    other mental problems 
-                    PLEASE GET PROFESSIONAL HELP</p>
-
-                <Link className="" to='/curse'>Curse Anonymously</Link>
+            <>
+            <div className='curseBlessWrapper'>
+            <Link componet={Curse}>
+                <Button className='curseText'><h2>Curse</h2>
+                </Button>
+                </Link>
+            {this.loggedInBlessPart()}
             </div>
+            </>
 
         )
     }
