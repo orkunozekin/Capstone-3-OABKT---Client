7187ec51a6857d01f08716d52ae80916c7cc1dec
Keith Freitag <keithfreitag@gmail.com>
2020-08-12 07:31:36 -0700

    getting there

--- src/components/CurseForm/CurseForm.js
+++ src/components/CurseForm/CurseForm.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
 import './CurseForm.css'
 
+
 export default class CurseForm extends Component {
+
+
+    // appearOnType = (curseInput) => {
+    //     document.getElementById("curseInput").innerHTML = curseInput.value;
+    // }
 
     render() {
         return (
+            <>
+            {/* <div className='inkBlots'></div> */}
+            
             <form onSubmit={this.props.handlePostCurses} className='curse-form'>
-                <textarea name='curseInput' className='curse-box'></textarea>
+                <textarea name='curseInput' className='curse-box' id='curseInput'></textarea>
                 <button className='void-button' type='submit'>Send into Void</button>
             </form>
+            <div className='scrollBottom'></div>
+            </>
         )
     }
 }
