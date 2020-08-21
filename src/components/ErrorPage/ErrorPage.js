7df72ac571892b15ce1a86194bcad099797e3d92
Thomas Davis <ThomasDavisA@gmail.com>
2020-08-21 09:08:02 -0700

    error page styled and mobile responsive

--- src/components/ErrorPage/ErrorPage.js
+++ src/components/ErrorPage/ErrorPage.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 // import {Link} from 'react-router-dom'
 import './keith.png'
 import './adam.png'
@@ -9,40 +9,39 @@ import './orkun.png'
 import './ErrorPage.css'
 import './nowHiring.png'
 
+const staffArray = ['keith', 'thomas', 'brian', 'adam', 'orkun', 'mike', 'fired'];
 export default class ErrorPage extends Component {
-  constructor(props){
+  constructor(props) {
     super(props)
     this.state = {
       complete: false,
-      
     }
   }
 
 
- handleFire = (e) => {
-   if (!this.state.complete) {
-     this.setState({complete: true})
-     document.getElementById(e.target.id).id = 'fired'
+
+  handleFire = (e) => {
+    if (!this.state.complete) {
+      this.setState({ complete: true })
+      document.getElementById(e.target.id).id = 'fired'
+    }
   }
-}
 
- 
 
-  render(){
 
-    return(
+  render() {
+    let staffClass = this.state.complete ? 'pic' : 'pic ready-to-fire';
+    return (
       <>
-      
-      <div className='errorWrapper'>
-      <div onClick={this.handleFire} id='keith' className='pic'></div>
-      <div onClick={this.handleFire} id='thomas' className='pic'></div>
-      <div onClick={this.handleFire} id='brian' className='pic'></div>
-      <div onClick={this.handleFire} id='adam' className='pic'></div>
-      <div onClick={this.handleFire} id='orkun' className='pic'></div>
-      <div onClick={this.handleFire} id='mike' className='pic'></div>
-      
-      
-      </div>
+
+        <div className='errorWrapper'>
+          <div onClick={this.handleFire} id='keith' className={staffClass}></div>
+          <div onClick={this.handleFire} id='thomas' className={staffClass}></div>
+          <div onClick={this.handleFire} id='brian' className={staffClass}></div>
+          <div onClick={this.handleFire} id='adam' className={staffClass}></div>
+          <div onClick={this.handleFire} id='orkun' className={staffClass}></div>
+          <div onClick={this.handleFire} id='mike' className={staffClass}></div>
+        </div>
       </>
     )
   }
--- src/routes/NotFoundRoute/NotFoundRoute.js
+++ src/routes/NotFoundRoute/NotFoundRoute.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react'
 import ErrorPage from '../../components/ErrorPage/ErrorPage'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import './NotFoundRoute.css'
 
 class NotFoundRoute extends Component {
   render() {
     return (
-      <>
-      <h2 className='errorh2'>You are somewhere you shouldn't be...</h2>
-      <Link className='errorlink' to='/'>Let's head home</Link>
-      <div className='ofCourse'><p>Of course, the customer is always right.  Which of these people 
+      <div className='error-route-container'>
+        <h2 className='errorh2'>You are somewhere you shouldn't be...</h2>
+        <Link className='errorlink' to='/'>Let's head home</Link>
+        <div className='ofCourse'>
+          <p>Of course, the customer is always right.  Which of these people
         gets fired for their mistake that brought you here?<br></br>
-        (Go ahead and click on the one we should fire)
-      </p>
+        (Go ahead and click on the one we should fire)</p>
+        </div>
+        <ErrorPage />
       </div>
-      <ErrorPage />
-      </>
     );
   }
 }
